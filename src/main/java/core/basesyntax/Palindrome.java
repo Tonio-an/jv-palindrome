1975faba8f3d2023317752a07429a0eca3a8e21b
Tonio-An <inncocentguy@gmail.com>
2020-04-01 20:29:38 +0300

    added solution

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,7 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        StringBuilder sb = new StringBuilder(text.replaceAll("[^a-zA-Z0-9]", "").toUpperCase());
+        return sb.toString().equals(sb.reverse().toString());
     }
 }
